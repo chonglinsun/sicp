a70c2e2a292aa584c9142975d359d3672026dbc7
chonglinsun <chonglinsun@gmail.com>
2013-02-01 13:58:14 -0500

    add erercise 2.77-2.80

--- ch2.scm
+++ ch2.scm
@@ -1872,4 +1872,152 @@
 	   (error "Unknown op MAKE-FROM-MAG-ANG" op))))
   dispatch)
 
+;;exercise 2.77
 ;;data-directed method would almost be a better choice
+
+;;content
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+
+(define (install-scheme-number-package)
+  (define (tag x)
+    (attach-tag 'scheme-number x))
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number
+       (lambda (x) (tag x)))
+  'done)
+
+(define (make-scheme-number n)
+  ((get 'make 'scheme-numner) n))
+
+(define (instsall-rational-package)
+  ;;internal procedures
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+    (let ((g (gcd n d)))
+      (cond (/ n g) (/ d g))))
+  (define (add-rat x y)
+    (make-rat (+ (* (numer x) (denom y))
+		 (* (numer y) (denom x)))
+	      (* (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+		 (* (numer y) (denom x)))
+	      (* (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+	      (* (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+	      (* (denom x) (numer y))))
+  ;;interface to rest of the system
+  (define (tag x) (attach-tag 'rational x))
+  (put 'add '(rational rational)
+       (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat x y))))
+  (put 'make 'rational 
+       (lambda (n d) (tag (make-rat n d))))
+  'done)
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
+
+(define (install-complex-package)
+  ;;imported procedures from rectangular and polar package
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'reacangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+  ;;internal peocedures
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+			 (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+			 (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+		       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+		       (- (angle z1) (angle z2))))
+  ;;interface to the rest of the system
+  (define (tag z) (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'make-from-real-imag 'complex
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+       (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (make-complex-from-real-imag x y)
+  ((get 'make-from-real-imag 'complex) x y))
+(define (make-complex-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'complex) r a))
+
+;;exercise 2.77
+;;twice, one for obtaining the complex package and
+;;another for obtaining the rectangular package
+
+;;exercise 2.78
+(define (attach-tag type-tag contents)
+  (if (eq? type-tag 'scheme-number)
+      contents
+      (cons type-tag contents)))
+
+(define (type-tag exp)
+  (cond ((pair? exp) (car exp))
+	((number? exp) 'scheme-number)
+	(else (error "Bad tagged exp -- TYPE-TAG" exp))))
+
+(define (contents exp)
+  (cond ((pair? exp) (cdr exp))
+	((number? exp) exp)
+	(else (error "Bad tagger exp -- CONTENTS" exp))))
+
+;;exercise 2.79
+;;(define (equ? x y) (apply-generic 'equ? x y))
+;;(put 'equ? '(scheme-number scheme-number) =)
+
+;;(define (equ-rat? x y) (and (equ? (number x) (number y))
+;;			   (equ? (denom x) (denom y))))
+;;(put 'equ? '(rational rational) equ-rat?)
+
+;;(define (equ-complex? z1 z2) (and (equ? (magnitude z1) (magnitude z2))
+;;				 (equ? (angle z1) (angle z2))))
+;;(put 'equ? '(complex complex) equ-complex?)
+
+;;exercise 2.80
+;;(define (=zero? x) (apply-generic '=zero? x))
+
+;;the scheme-number package
+;;(put '=zero '(scheme-number) zero?)
+
+;;the rational package
+;;(define (=zero? x) (zero? (numer x)))
+;;(put 'zero? '(rational) =zero?)
+
+;;the complex package
+;;(define (=zero? z1) (zero? (magnitude z1)))
+;;(put '=zero? '(complex) =zero?)
